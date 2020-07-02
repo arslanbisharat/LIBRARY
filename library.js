3ef2731edde3241432dbc8efc38e2836802f4bb3
Muhammad Arslan <thearslan380@gmail.com>
2020-07-02 02:40:09 -0700

    Update library.js

--- library.js
+++ library.js
@@ -29,38 +29,11 @@ function updateLibraryStore (books) {
   }
 }
 
-class Library {
-  constructor(){
-    this.books = [];
-  };
-  
-  addBook (book) {
-    this.books.push(book);
-    updateLibraryStore(this.books);
-  }
 
-  removeBook (index) {
-    this.books.splice(index, 1);
-    updateLibraryStore(this.books);
-  }
-}
-
-class Book {
-  constructor(title, author, pageNumber, read) {
-    this.title = title;
-    this.author = author;
-    this.pageNumber = pageNumber;
-    this.read = read;
-  }
-
-  info () {
-    return `${this.title} by ${this.author}, ${this.pageNumber} pages, ${this.read}`;
-  }
-}
 
 function render() {
   // Create new table content
-  const virtualContent = document.createElement('tbody');
+  const virtualContent = document.createElement('tbody')
 
   // Update table
   myLibrary.books.forEach((book, index) => {
