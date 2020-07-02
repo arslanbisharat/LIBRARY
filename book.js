09a972400c63467eceb689268320af1955d1154c
Muhammad Arslan <thearslan380@gmail.com>
2020-07-02 02:40:48 -0700

    Update book.js

--- book.js
+++ book.js
@@ -1,3 +1,19 @@
+class Library {
+  constructor(){
+    this.books = [];
+  };
+  
+  addBook (book) {
+    this.books.push(book);
+    updateLibraryStore(this.books);
+  }
+
+  removeBook (index) {
+    this.books.splice(index, 1);
+    updateLibraryStore(this.books);
+  }
+}
+
 class Book {
   constructor(title, author, pageNumber, read) {
     this.title = title;
@@ -5,3 +21,8 @@ class Book {
     this.pageNumber = pageNumber;
     this.read = read;
   }
+
+  info () {
+    return `${this.title} by ${this.author}, ${this.pageNumber} pages, ${this.read}`;
+  }
+}
